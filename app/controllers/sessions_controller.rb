9d4684fb2719c3c77017433aac09d8a093954a07
Temuujin Natsagnyam <temuujin.nat@gmail.com>
2025-06-15 18:30:42 +0100

    Add registration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
+    if (user = User.authenticate_by(params.permit(:email_address, :password)))
       start_new_session_for user
       redirect_to after_authentication_url
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      flash.now[:alert] = "Try another email address or password."
+      render :new, status: :unprocessable_entity
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_15_155936) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_15_161309) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -25,6 +25,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_15_155936) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name"
+    t.string "username", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
